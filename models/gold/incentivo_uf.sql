229df1bed57b686d52f4570466ba44af9913ea3c
Anselmo Moraes <anselmo@rescuepoint.com.br>
2024-09-16 15:14:19 +0000

    Ajuste no incentivo_uf

--- models/gold/incentivo_uf.sql
+++ models/gold/incentivo_uf.sql
@@ -2,11 +2,11 @@
 
 WITH INCENTIVO_UF AS (
     SELECT 
-        UF_INCENTIVO,
+        UF_PROJETO,
         SUM(VL_INCENTIVO) AS TOTAL_INCENTIVO
     FROM {{ ref('dados_salic_silver') }}
     GROUP BY
-        UF_INCENTIVO
+        UF_PROJETO
     ORDER BY
         TOTAL_INCENTIVO DESC
     LIMIT 5
